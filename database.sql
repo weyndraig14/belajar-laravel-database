d64b53a28204c9232cd4c90d1197b22b6fc59fe4
Agung Timotius Sinaga <agungsinaga2007@gmail.com>
2024-04-21 13:21:11 +0700

    have completed the learning material about laravel database

---
+++ database.sql
@@ -0,0 +1,49 @@
+-- Active: 1713455577692@@127.0.0.1@3306@belajar_laravel_database
+create database belajar_laravel_database;
+
+use belajar_laravel_database;
+
+create table categories
+(
+    id          varchar(100) not null primary key,
+    name        varchar(100) not null,
+    description text,
+    created_at  timestamp
+) engine innodb;
+
+desc categories;
+
+create table counters
+(
+    id      varchar(100) not null primary key,
+    counter int not null default 0
+) engine innodb;
+
+insert into counters(id, counter) values ('sample', 0);
+
+select * from counters;
+
+create table products
+(
+    id          varchar(100) not null primary key,
+    name        varchar(100) not null,
+    description text null,
+    price       int not null,
+    category_id varchar(100) not null,
+    created_at  timestamp not null default current_timestamp,
+    constraint  fk_category_id foreign key (category_id) references categories (id)
+) engine innodb;
+
+select * from categories;
+
+drop table products;
+
+drop table categories;
+
+drop table counters;
+
+show tables;
+
+select * from migrations;
+
+select * from categories;
